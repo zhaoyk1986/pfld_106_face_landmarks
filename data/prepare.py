31a35b2ed273336d9bde89c50ca3ea856434a3ae
Hsintao <xintaokevin@gmail.com>
2020-08-10 18:12:17 +0800

    😎

---
+++ data/prepare.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python
+# @File    : prepare.py
+# @Time    : 2020/4/10
+# @Author  : Xintao
+
+import os
+import numpy as np
+import cv2
+import shutil
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "..")))
+
+
+def calculate_pitch_yaw_roll(landmarks_2D, cam_w=256, cam_h=256, radians=False):
+    """ Return the the pitch  yaw and roll angles associated with the input image.
+    @param radians When True it returns the angle in radians, otherwise in degrees.
+    """
+
+    assert landmarks_2D is not None, 'landmarks_2D is None'
+
+    # Estimated camera matrix values.
+    c_x = cam_w / 2
+    c_y = cam_h / 2
+    f_x = c_x / np.tan(60 / 2 * np.pi / 180)
+    f_y = f_x
+    camera_matrix = np.float32([[f_x, 0.0, c_x], [0.0, f_y, c_y],
+                                [0.0, 0.0, 1.0]])
+    camera_distortion = np.float32([0.0, 0.0, 0.0, 0.0, 0.0])
+
+    # dlib (68 landmark) trached points
+    # TRACKED_POINTS = [17, 21, 22, 26, 36, 39, 42, 45, 31, 35, 48, 54, 57, 8]
+    # wflw(98 landmark) trached points
+    # TRACKED_POINTS = [33, 38, 50, 46, 60, 64, 68, 72, 55, 59, 76, 82, 85, 16]
+    # X-Y-Z with X pointing forward and Y on the left and Z up.
+    # The X-Y-Z coordinates used are like the standard coordinates of ROS (robotic operative system)
+    # OpenCV uses the reference usually used in computer vision:
+    # X points to the right, Y down, Z to the front
+    landmarks_3D = np.float32([
+        [6.825897, 6.760612, 4.402142],  # LEFT_EYEBROW_LEFT,
+        [1.330353, 7.122144, 6.903745],  # LEFT_EYEBROW_RIGHT,
+        [-1.330353, 7.122144, 6.903745],  # RIGHT_EYEBROW_LEFT,
+        [-6.825897, 6.760612, 4.402142],  # RIGHT_EYEBROW_RIGHT,
+        [5.311432, 5.485328, 3.987654],  # LEFT_EYE_LEFT,
+        [1.789930, 5.393625, 4.413414],  # LEFT_EYE_RIGHT,
+        [-1.789930, 5.393625, 4.413414],  # RIGHT_EYE_LEFT,
+        [-5.311432, 5.485328, 3.987654],  # RIGHT_EYE_RIGHT,
+        [-2.005628, 1.409845, 6.165652],  # NOSE_LEFT,
+        [-2.005628, 1.409845, 6.165652],  # NOSE_RIGHT,
+        [2.774015, -2.080775, 5.048531],  # MOUTH_LEFT,
+        [-2.774015, -2.080775, 5.048531],  # MOUTH_RIGHT,
+        [0.000000, -3.116408, 6.097667],  # LOWER_LIP,
+        [0.000000, -7.415691, 4.070434],  # CHIN
+    ])
+    landmarks_2D = np.asarray(landmarks_2D, dtype=np.float32).reshape(-1, 2)
+
+    # Applying the PnP solver to find the 3D pose of the head from the 2D position of the landmarks.
+    # retval - bool
+    # rvec - Output rotation vector that, together with tvec, brings points from the world coordinate system to the camera coordinate system.
+    # tvec - Output translation vector. It is the position of the world origin (SELLION) in camera co-ords
+    _, rvec, tvec = cv2.solvePnP(landmarks_3D, landmarks_2D,
+                                 camera_matrix, camera_distortion)
+    # Get as input the rotational vector, Return a rotational matrix
+
+    # const double PI = 3.141592653;
+    # double thetaz = atan2(r21, r11) / PI * 180;
+    # double thetay = atan2(-1 * r31, sqrt(r32*r32 + r33*r33)) / PI * 180;
+    # double thetax = atan2(r32, r33) / PI * 180;
+
+    rmat, _ = cv2.Rodrigues(rvec)
+    pose_mat = cv2.hconcat((rmat, tvec))
+    _, _, _, _, _, _, euler_angles = cv2.decomposeProjectionMatrix(pose_mat)
+    return map(lambda k: k[0], euler_angles)  # euler_angles contain (pitch, yaw, roll)
+
+
+def rotate(angle, center, landmark):
+    rad = angle * np.pi / 180.0
+    alpha = np.cos(rad)
+    beta = np.sin(rad)
+    M = np.zeros((2, 3), dtype=np.float32)
+    M[0, 0] = alpha
+    M[0, 1] = beta
+    M[0, 2] = (1 - alpha) * center[0] - beta * center[1]
+    M[1, 0] = -beta
+    M[1, 1] = alpha
+    M[1, 2] = beta * center[0] + (1 - alpha) * center[1]
+
+    landmark_ = np.asarray([(M[0, 0] *
+                             x +
+                             M[0, 1] *
+                             y +
+                             M[0, 2], M[1, 0] *
+                             x +
+                             M[1, 1] *
+                             y +
+                             M[1, 2]) for (x, y) in landmark])
+    return M, landmark_
+
+
+class ImageDate:
+    def __init__(self, line, imgDir, image_size=112):
+        self.image_size = image_size
+        line = line.strip().split()
+
+        assert(len(line) == 217)  # 1 + 4 + 106 * 2
+        self.list = line
+        self.landmark = np.asarray(
+            list(map(float, line[5:])), dtype=np.float32).reshape(-1, 2)
+        self.box = np.asarray(list(map(int, line[1:5])), dtype=np.int32)
+        self.path = os.path.join(imgDir, line[0])
+        self.img = None
+
+        self.imgs = []
+        self.landmarks = []
+        self.boxes = []
+
+    def load_data(self, is_train, repeat):
+        xy = np.min(self.landmark, axis=0).astype(np.int32)
+        zz = np.max(self.landmark, axis=0).astype(np.int32)
+        wh = zz - xy + 1
+
+        center = (xy + wh / 2).astype(np.int32)
+        img = cv2.imread(self.path)
+        boxsize = int(np.max(wh) * 1.2)  # 关键点区域扩张1.2倍
+        xy = center - boxsize // 2
+        x1, y1 = xy
+        x2, y2 = xy + boxsize
+        height, width, _ = img.shape
+        dx = max(0, -x1)
+        dy = max(0, -y1)
+        x1 = max(0, x1)
+        y1 = max(0, y1)
+
+        edx = max(0, x2 - width)
+        edy = max(0, y2 - height)
+        x2 = min(width, x2)
+        y2 = min(height, y2)
+
+        imgT = img[y1:y2, x1:x2]
+        if dx > 0 or dy > 0 or edx > 0 or edy > 0:
+            imgT = cv2.copyMakeBorder(
+                imgT, dy, edy, dx, edx, cv2.BORDER_CONSTANT, 0)
+        if imgT.shape[0] == 0 or imgT.shape[1] == 0:
+            imgTT = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+            for x, y in (self.landmark + 0.5).astype(np.int32):
+                cv2.circle(imgTT, (x, y), 1, (0, 0, 255))
+            cv2.imshow('0', imgTT)
+            if cv2.waitKey(0) == 27:
+                exit()
+        imgT = cv2.resize(imgT, (self.image_size, self.image_size))
+        landmark = (self.landmark - xy) / boxsize  # 裁剪后 归一化
+        assert (landmark >= 0).all(), str(landmark) + str([dx, dy])
+        assert (landmark <= 1).all(), str(landmark) + str([dx, dy])
+        self.imgs.append(imgT)
+        self.landmarks.append(landmark)
+
+        if is_train:
+            while len(self.imgs) < repeat:  # 对训练数据 repeat次增广
+                angle = np.random.randint(-30, 30)
+                cx, cy = center
+                cx = cx + int(np.random.randint(-boxsize * 0.1, boxsize * 0.1))
+                cy = cy + int(np.random.randint(-boxsize * 0.1, boxsize * 0.1))
+                M, landmark = rotate(angle, (cx, cy), self.landmark)
+
+                imgT = cv2.warpAffine(
+                    img, M, (int(img.shape[1] * 1.1), int(img.shape[0] * 1.1)))
+
+                wh = np.ptp(landmark, axis=0).astype(np.int32) + 1
+                size = np.random.randint(
+                    int(np.min(wh)), np.ceil(np.max(wh) * 1.25))
+                xy = np.asarray(
+                    (cx - size // 2, cy - size // 2), dtype=np.int32)
+                landmark = (landmark - xy) / size
+                if (landmark < 0).any() or (landmark > 1).any():
+                    continue
+
+                x1, y1 = xy
+                x2, y2 = xy + size
+                height, width, _ = imgT.shape
+                dx = max(0, -x1)
+                dy = max(0, -y1)
+                x1 = max(0, x1)
+                y1 = max(0, y1)
+
+                edx = max(0, x2 - width)
+                edy = max(0, y2 - height)
+                x2 = min(width, x2)
+                y2 = min(height, y2)
+
+                imgT = imgT[y1:y2, x1:x2]
+                if dx > 0 or dy > 0 or edx > 0 or edy > 0:
+                    imgT = cv2.copyMakeBorder(
+                        imgT, dy, edy, dx, edx, cv2.BORDER_CONSTANT, 0)
+
+                imgT = cv2.resize(imgT, (self.image_size, self.image_size))
+
+                # if mirror is not None and np.random.choice((True, False)):
+                #     landmark[:, 0] = 1 - landmark[:, 0]
+                #     landmark = landmark[mirror_idx]
+                #     imgT = cv2.flip(imgT, 1)
+                self.imgs.append(imgT)
+                self.landmarks.append(landmark)
+
+    def save_data(self, path, prefix):
+
+        labels = []
+        TRACKED_POINTS = [43, 46, 97, 101,
+                          35, 75, 89, 93,
+                          77, 83, 52, 61,
+                          53, 0]
+        for i, (img, lanmark) in enumerate(zip(self.imgs, self.landmarks)):
+            assert lanmark.shape == (106, 2)
+            save_path = os.path.join(path, prefix + '_' + str(i) + '.jpg')
+            assert not os.path.exists(save_path), save_path
+            cv2.imwrite(save_path, img)
+
+            euler_angles_landmark = []
+            for index in TRACKED_POINTS:
+                euler_angles_landmark.append(lanmark[index])
+            euler_angles_landmark = np.asarray(
+                euler_angles_landmark).reshape((-1, 28))
+            pitch, yaw, roll = calculate_pitch_yaw_roll(
+                euler_angles_landmark[0])
+            euler_angles = np.asarray((pitch, yaw, roll), dtype=np.float32)
+            euler_angles_str = ' '.join(list(map(str, euler_angles)))
+
+            landmark_str = ' '.join(
+                list(map(str, lanmark.reshape(-1).tolist())))
+
+            label = '{} {} {} \n'.format(save_path, landmark_str, euler_angles_str)
+            labels.append(label)
+        return labels
+
+
+def get_dataset_list(imgDir, outDir, landmarkFile, is_train, test_ratio=0.1):
+    with open(landmarkFile, 'r') as f:
+        lines = f.readlines()
+        labels = []
+        save_img = os.path.join(outDir, 'imgs')
+        if not os.path.exists(save_img):
+            os.mkdir(save_img)
+        num_train = int(len(lines) * (1 - test_ratio))
+        if is_train:
+            lines = lines[:num_train]
+        else:
+            lines = lines[num_train:]
+
+        for i, line in enumerate(lines):
+            Img = ImageDate(line, imgDir)
+            img_name = Img.path
+            Img.load_data(is_train, 5)
+            _, filename = os.path.split(img_name)
+            filename, _ = os.path.splitext(filename)
+            label_txt = Img.save_data(save_img, str(i) + '_' + filename)
+            labels.append(label_txt)
+            if ((i + 1) % 100) == 0:
+                print('file: {}/{}'.format(i + 1, len(lines)))
+
+    with open(os.path.join(outDir, 'list.txt'), 'w') as f:
+        for label in labels:
+            f.writelines(label)  # 一次写入多行字符串
+
+
+if __name__ == '__main__':
+    root_dir = os.path.dirname(os.path.realpath(__file__))
+    imageDirs = './imgs'
+
+    landmarkFile = os.path.join(root_dir, "bbox_landmark.txt")
+
+    outDirs = ['test_data', 'train_data']
+    for outDir in outDirs:
+        outDir = os.path.join(root_dir, outDir)
+        print(outDir)
+        if os.path.exists(outDir):
+            shutil.rmtree(outDir)
+        os.mkdir(outDir)
+        if 'test' in outDir:
+            is_train = False
+        else:
+            is_train = True
+        get_dataset_list(imageDirs, outDir, landmarkFile, is_train, test_ratio=0.1)
+    print('end')
